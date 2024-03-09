9f1f959f14218b4c0d4a35bdd4eb47f5d3419d68
Prudhvi <47024310+prudhvisai26@users.noreply.github.com>
2024-03-09 21:50:52 +0530

    Create CountPrimes.py

---
+++ Bit Manipulation/Advanced/CountPrimes.py
@@ -0,0 +1,28 @@
+"""
+Problem statement
+
+You are given an integer 'N'.
+You must return the unique prime factors of 'N' in increasing order.
+
+For Example:
+For ‘N’ = 10.
+Unique prime factors are 2 and 5.
+Hence we return {2, 5}.
+"""
+
+def countPrimes(n: int) -> int:
+    # Write your code here.
+    s=[1]*(n+1)
+    s[0]=0
+    s[1]=0
+    for i in range(2,n+1):
+        if s[i]:
+            j=i*i
+            while j<=n:
+                s[j]=0
+                j=j+i
+    ans=[]
+    for i in range(2,n+1):
+        if s[i] and n%i==0:
+            ans.append(i)
+    return ans
